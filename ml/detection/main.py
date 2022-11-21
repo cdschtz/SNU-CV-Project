7d07567ecc8dbdea4dc34ad443925dae0c9d23f1
Christopher Schütz <schuetzchristopher@icloud.com>
2022-11-21 16:49:36 +0900

    use argparse for main.py execution logic

--- ml/detection/main.py
+++ ml/detection/main.py
@@ -1,9 +1,14 @@
+import argparse
+
 from detector import Detector
 from utils import MODEL_VARIANT
 
 if __name__ == '__main__':
-    detector = Detector(model_variant=MODEL_VARIANT.SSD_LITE)
-    detector.detect(
-        "./ml/detection/data/test_short_video.mov",
-        save_file_name="test_short_video.mp4"
-    )
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', help='Path to the video file', type=str)
+    parser.add_argument('--model', type=str,
+                        default=MODEL_VARIANT.SSD_LITE, help='Model name')
+    args = parser.parse_args()
+
+    detector = Detector(model_variant=MODEL_VARIANT(args.model))
+    detector.detect(args.file)
